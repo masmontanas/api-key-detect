29ea6b271930d036c60d99e64345d903af5d9b97
Alek Jouharyan <alekjouharyan@gmail.com>
2018-11-30 13:04:52 -0800

    adding tests

---
+++ tests.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+from api_key_detect import scan_for_keys
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class TestCase(unittest.TestCase):
+
+    def test_api_key_detection(self):
+        key_count = scan_for_keys(basedir)
+        assert key_count >= 3
+
+
+if __name__ == '__main__':
+    try:
+        unittest.main()
+    except:
+        pass
